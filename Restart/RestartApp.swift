05a41c7c19a13c8275255e88072988dbb142d458
Hiroyuki Shoji <hiroyuki.shoji@sony.com>
2023-02-28 21:43:53 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 小路宏幸 on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
